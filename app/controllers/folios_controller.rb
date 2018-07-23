db0ee067385c6c3f535776562476e69b8cd1703c
jimboqu <jamesm.quilter@gmail.com>
2018-07-23 20:48:36 +0100

    new and create

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -2,4 +2,27 @@ class FoliosController < ApplicationController
 	def index
 	  @folio_items =  Folio.all
 	end
+
+
+	def new
+	  @folio_item = Folio.new
+	end
+
+	def create
+      @folio_item = Folio.new(folio_params)
+      if @folio_item.save
+      	redirect_to @folio_item
+      else
+      	render 'new'
+      end
+	end
+
+	def show
+	end
+
+	private
+
+	  def folio_params
+        params.require(:folio).permit(:title, :subtitle, :body)
+	  end
 end
