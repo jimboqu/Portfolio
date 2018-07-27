73f2926bd07cdd5f876417976fcfbe6a5152ffbf
jimboqu <jamesm.quilter@gmail.com>
2018-07-27 16:32:53 +0100

    Blog published link

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'folio/:id', to: 'folios#show', as: 'folio_show'
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
