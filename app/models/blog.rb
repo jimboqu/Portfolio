05baf9aa9275059d2c4df5c3a24274895c39b7bc
jimboqu <jamesm.quilter@gmail.com>
2018-07-27 22:14:00 +0100

    added validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
   extend FriendlyId
   enum status: {draft: 0, published: 1}
   friendly_id :title, use: :slugged
+  validates_presence_of :title, :body
 end
--- app/models/folio.rb
+++ app/models/folio.rb
@@ -1,2 +1,3 @@
 class Folio < ApplicationRecord
+  validates_presence_of :title, :subtitle, :main_image, :thumb_image, :body
 end
