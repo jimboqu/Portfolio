550cb4bf7f71ea1ef7de09163b31dafe377523af
jimboqu <jamesm.quilter@gmail.com>
2018-07-30 16:11:35 +0100

    added custom field for devise on name for new and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :config_devise_params, if: :devise_controller?
+  #this is to allow strong params in devise for name
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
 end
