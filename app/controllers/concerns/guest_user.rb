e58ad93e5003ad2042210a58c562512553d562a9
jimboqu <jamesm.quilter@gmail.com>
2018-07-31 17:09:50 +0100

    slightrefactor on guest concern

--- app/controllers/concerns/guest_user.rb
+++ app/controllers/concerns/guest_user.rb
@@ -7,7 +7,14 @@ included do
 end
 
 def current_user
-  super || OpenStruct.new(name:"Guest User", first_name: "Guest", last_name: 'User', email: "guest@guest.com")
+  super || guest_user
+end
+
+def guest_user
+  OpenStruct.new(name:"Guest User", 
+  	first_name: "Guest", 
+  	last_name: 'User', 
+  	email: "guest@guest.com")
 end
 
 end
