009ae4f8942dcaedb4b23de9eb5c3925066b6e99
jimboqu <jamesm.quilter@gmail.com>
2018-08-03 13:48:04 +0100

    added gem that isnt a gem

--- Gemfile
+++ Gemfile
@@ -55,6 +55,6 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-gem 'jimboqu_copyright', git: "https://github.com/jimboqu/jimboqu_copyright"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,12 @@ class ApplicationController < ActionController::Base
     @copyright = JimboquCopyright::Renderer.copyright("James", "Reserved")
   end
 
-  
+  module JimboquCopyright 
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year}  #{name}. #{msg}".html_safe
+      end
+    end
+  end
   
 end
