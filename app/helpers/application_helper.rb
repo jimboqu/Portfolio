6f4535f8895040964b61309e926b82c9dab4dc7b
jimboqu <jamesm.quilter@gmail.com>
2018-08-10 16:23:30 +0100

    Fixed nav with active style and one helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,19 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting") 
     end 
   end
+#the method below takes two arguments and makes a nav. 
+  def navigation_helper style, tag
+nav_links = <<NAV
+<#{tag}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag}>
+<#{tag}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag}>
+<#{tag}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag}>
+<#{tag}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag}>
+<#{tag}><a href="#{folios_path}" class="#{style} #{active? folios_path}">Folio</a></#{tag}>
+NAV
+  nav_links.html_safe
+  end
+#below checks if you're on that path. Active is a class. It gets inserted in quotes in method
+  def active? path 
+    "active" if current_page? path
+  end
 end
