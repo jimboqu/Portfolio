6eb4f2506bad24c3ee1b58ec23cf6fdc1228ba08
jimboqu <jamesm.quilter@gmail.com>
2018-08-19 08:54:55 +0100

    added in Twitter authentication and pulling in tweets

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,10 +1,15 @@
 module SocialTool
   def self.twitter_search
 	client = Twitter::REST::Client.new do |config|
-	  config.consumer_key        = "n2mpW2frAtKZgCB6qSWDcSexq"
-	  config.consumer_secret     = "jmC3mXaKymqLSHSWJtfUZaqWKlsWKiyEU5xiL9Ks3NKVhoJ3Dy"
-	  config.access_token        = "3696081981-RqLyG19BB16vMHVLFJwuSCvQcNZvB0X1LYBNYZu"
-	  config.access_token_secret = "WUpNsNtoKnjc4UGqHQnnp3syGSouwZ0lCMu8Lq4qlGoF1"
+	  config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
+	  config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
+	  config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
+	  config.access_token_secret = ENV.fetch("TWITTER_ACCESS_TOKEN_SECRET")
+	end
+
+	client.search("Content Marketing Tools", result_type: "recent").take(6).collect do |tweet|
+	  "#{tweet.user.screen_name}: #{tweet.text}"
 	end
   end
 end
+
