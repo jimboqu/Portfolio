7a85d07b9076108d31afc918d9c38b386b5c76cd
jimboqu <jamesm.quilter@gmail.com>
2018-09-03 13:20:23 +0100

    topics and form for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   enum status: {draft: 0, published: 1}
   friendly_id :title, use: :slugged
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic_id
   belongs_to :topic
   has_many :comments, dependent: :destroy
 end
