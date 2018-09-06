16666b1a65331b449ce5f86148c37fe5901ed6fd
jimboqu <jamesm.quilter@gmail.com>
2018-09-06 11:58:56 +0100

    change js order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require html.sortable
@@ -21,3 +20,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
